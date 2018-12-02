284213c6f2a03f69eef4add1fda712046033ddcb
i05nagai <i05nagai@gmail.com>
2018-12-02 15:03:00 +0900

    Update scripts for Linux environment
    These files should be simultaneously updated with the commit da3b26aa09df3da8d0c603ced46cf3e6ab00164a

--- ci/linux/install_clang_format.sh
+++ ci/linux/install_clang_format.sh
@@ -6,6 +6,6 @@ if [[ ! -z ${RECIPE_DEBUG+x} ]]; then
 fi
 
 PATH_TO_CI=$(cd $(dirname ${0});cd ..;pwd)
-source ${PATH_TO_CI}/_lib/common_option.sh
+source ${PATH_TO_CI}/_lib/install_clang_format.sh
 
-_use_sudo=${_use_sudo} _do_update=${_do_update} install_clang_format_osx
+_use_sudo=${_use_sudo} _do_update=${_do_update} install_clang_format_linux
