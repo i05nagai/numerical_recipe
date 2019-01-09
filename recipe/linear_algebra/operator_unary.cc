553e15f166e7a529c64a1934b951219692b29cf9
i05nagai <i05nagai@gmail.com>
2019-01-09 18:18:41 +0900

    Add function to transpose matrix

--- recipe/linear_algebra/operator_unary.cc
+++ recipe/linear_algebra/operator_unary.cc
@@ -1,5 +1,6 @@
 #include "recipe/linear_algebra/operator_unary.h"
 #include <cmath>
+#include "recipe/linear_algebra/base.h"
 
 namespace recipe {
 namespace linear_algebra {
@@ -14,6 +15,17 @@ double NormEuclid(const double* vec, const int size) {
 double NormEuclid(const Vector vec)
 {
   return NormEuclid(vec.Get(), vec.Size());
+}
+
+std::unique_ptr<double[]> Transpose(const double* mat, const int row_size, const int col_size)
+{
+  std::unique_ptr<double[]> data(new double[row_size * col_size]);
+  for (int row = 0; row < row_size; ++row) {
+    for (int col = 0; col < col_size; ++col) {
+      MATRIX(data, row_size, col, row) = MATRIX(mat, col_size, row, col);
+    }
+  }
+  return data;
 }
 }  // namespace linear_algebra
 }  // namespace recipe
