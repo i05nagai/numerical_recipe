b34eafabf91aeab6bfd7a0183b723d05207b02ae
“Ryo <asano.r.aa@gmail.com>
2019-03-31 19:22:12 +0900

    format files.

--- recipe/quadrature/richardson_extrapolation.h
+++ recipe/quadrature/richardson_extrapolation.h
@@ -1,18 +1,17 @@
-namespace recipe{
+namespace recipe {
 namespace quadrature {
 /*! \enum QuadratureRule
  *
- *  QuadratureRule enumerator lists all the quadrature rules that have been implemented.
- *  Avairable are: Trapezoidal, Simpson.
- *  Please put a name in the definition when you implements a new quadrature rule.
+ *  QuadratureRule enumerator lists all the quadrature rules that have been
+ * implemented. Avairable are: Trapezoidal, Simpson. Please put a name in the
+ * definition when you implements a new quadrature rule.
  *
  */
-enum QuadratureRule {
-    Trapezoidal,
-    Simpson
-};
+enum QuadratureRule { Trapezoidal, Simpson };
 
-double RichardsonExtrapolation(QuadratureRule quadrature_rule, double (*integrand)(double),
-        double left_endpoint, double right_endpoint, unsigned int num_of_partition);
-}//namespace richardson
-}//namespace recipe
+double RichardsonExtrapolation(QuadratureRule quadrature_rule,
+                               double (*integrand)(double),
+                               double left_endpoint, double right_endpoint,
+                               unsigned int num_of_partition);
+}  // namespace quadrature
+}  // namespace recipe
