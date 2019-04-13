97088e8f81fea464fee1dc6412481036ffe733e2
i05nagai <i05nagai@gmail.com>
2019-04-13 18:38:00 +0200

    Update specify path to golang explicitly

--- ci/_lib/install_buildifier.sh
+++ ci/_lib/install_buildifier.sh
@@ -41,7 +41,7 @@ install_buildifier_linux()
   ${SUDO} apt-get install -y \
         git \
         golang-1.10-go
-  ${SUDO} go get github.com/bazelbuild/buildtools/buildifier
+  ${SUDO} /usr/lib/go-1.10/bin/go get github.com/bazelbuild/buildtools/buildifier
 }
 
 #
