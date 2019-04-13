5a13f2f0feacdb0f43a881da8aa018db3918ce38
i05nagai <i05nagai@gmail.com>
2019-04-13 12:30:19 +0200

    Fix the names of functions

--- ci/_lib/install_buildifier.sh
+++ ci/_lib/install_buildifier.sh
@@ -12,7 +12,7 @@ fi
 #   * _do_update=1
 #   * _use_sudo=1
 #
-install_bazel_linux()
+install_buildifier_linux()
 {
 
   if [[ ${_use_sudo} -eq 1 ]]; then
@@ -53,7 +53,7 @@ install_bazel_linux()
 # variables:
 #   * _do_update=1
 #
-install_bazel_osx()
+install_buildifier_osx()
 {
   if [[ ${_do_update} -eq 1 ]]; then
     brew update
