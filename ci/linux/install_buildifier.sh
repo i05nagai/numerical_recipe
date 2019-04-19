c6e6a6dc9a25fc3604525c9a7082f8b851e17881
i05nagai <i05nagai@gmail.com>
2019-04-19 21:12:35 +0200

    Fix typos

--- ci/linux/install_buildifier.sh
+++ ci/linux/install_buildifier.sh
@@ -8,4 +8,4 @@ fi
 PATH_TO_CI=$(cd $(dirname ${0});cd ..;pwd)
 source ${PATH_TO_CI}/_lib/install_buildifier.sh
 
-_use_sudo=${_use_sudo} _do_update=${_do_update} install_bazel_linux
+_use_sudo=${_use_sudo} _do_update=${_do_update} install_buildifier_linux
